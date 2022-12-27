564bb1a3dedf151db4e0c7a3aa31bb05b4b3e364
Tanasov Mykhail <mtanasov@gmail.com>
2022-12-27 19:46:20 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Welcome } from '../components/Welcome/Welcome';
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+
+export default function HomePage() {
+  return (
+    <>
+      Hello mantine
+      <Welcome />
+      <ColorSchemeToggle />
+    </>
+  );
+}
